ff51d2ad37f195448228da4510697a751d158aa7
Sebastian Delage <108374679+SebastianDelage@users.noreply.github.com>
2026-01-21 23:40:00 -0300

    -creation

---
+++ sERPent/src/main/java/com/serpent/serpent/sERPentApplication.java
@@ -0,0 +1,13 @@
+package com.serpent.serpent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SErPentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SErPentApplication.class, args);
+    }
+
+}
---
+++ sERPent/src/test/java/com/serpent/serpent/SErPentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.serpent.serpent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class sERPentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
