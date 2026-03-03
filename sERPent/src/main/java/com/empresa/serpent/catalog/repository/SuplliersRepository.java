9ebc2fee942d4d7e5f870e905353dad8b82df2e4
Ignacio Dente <ignacioprogramando@gmail.com>
2026-03-02 23:35:13 -0300

    -refactor(inventory) change repository directory

--- sERPent/src/main/java/com/empresa/serpent/catalog/repository/SuplliersRepository.java
+++ sERPent/src/main/java/com/empresa/serpent/catalog/repository/SuplliersRepository.java
@@ -1,4 +1,4 @@
-package com.serpent.serpent.catalog.repository;
+package com.empresa.serpent.catalog.repository;
 
 import com.empresa.serpent.catalog.domain.Suppliers;
 import org.springframework.data.jpa.repository.JpaRepository;
