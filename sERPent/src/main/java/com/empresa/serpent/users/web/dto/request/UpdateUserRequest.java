a4ac818e0d1ed8e284efc281d2a970a985e97535
Ignacio Dente <ignacioprogramando@gmail.com>
2026-03-08 20:33:03 -0300

    refactor(users): add email format validation

--- sERPent/src/main/java/com/empresa/serpent/users/web/dto/request/UpdateUserRequest.java
+++ sERPent/src/main/java/com/empresa/serpent/users/web/dto/request/UpdateUserRequest.java
@@ -1,16 +1,24 @@
 package com.empresa.serpent.users.web.dto.request;
 
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 
 public record UpdateUserRequest(
+
         @NotBlank(message = "Name cannot be blank")
         String name,
+
         String lastName,
+
         @NotBlank(message = "Username cannot be blank")
         String username,
+
         @NotBlank(message = "Password hash cannot be blank")
         String passwordHash,
+
+        @Email(message = "Email must be a valid email address")
         String email,
+
         Boolean active
 ) {
 }
